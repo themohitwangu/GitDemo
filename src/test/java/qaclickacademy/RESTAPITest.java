91f3829e5fc478be74b205d80d4d989f13525f94
mohit <themohitwangu@gmail.com>
2020-06-12 08:29:43 +0530

    Commit from Asian Time Zone person

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -9,6 +9,10 @@ public class RESTAPITest
 	public void postJira()
 	{
 		System.out.println("postJira");
+		System.out.println("postJira-2");
+		System.out.println("postJira-3");
+		System.out.println("postJira-4");
+		System.out.println("postJira-5");
 	}
 
 	
