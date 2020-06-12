61e2140120b965adb812889a41d8c254741ed0e8
mohit <themohitwangu@gmail.com>
2020-06-12 07:42:48 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest 
+{
+	//public WebDriver driver;
+	
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+
+	
+	@Test
+	public void iOSApps()
+	{
+		System.out.println("iOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest
+{
+
+	@Test
+	public void Browser_Automation()
+	{
+		System.out.println("Browser_Automation");
+	}
+	
+	
+	@Test
+	public void Elements_UI_Automation()
+	{
+		System.out.println("Elements_UI_Automation");
+	}
+	
+}
